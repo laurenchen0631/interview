eb6f15da2a07145806c4505b7c48d934316319a2
Lauren Chen <laurenchen0631@gmail.com>
2022-01-11 20:48:11 -0800

    701

---
+++ leetcode/python/701_insert_bst.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def insertIntoBST(self, root: TreeNode | None, val: int) -> TreeNode | None:
+        if not root:
+            return TreeNode(val)
+
+        node = root
+        while node:
+            if val < node.val:
+                if not node.left:
+                    node.left = TreeNode(val)
+                    break
+                else:
+                    node = node.left
+                
+            else:
+                if not node.right:
+                    node.right = TreeNode(val)
+                    break
+                else:
+                    node = node.right
+        return root
