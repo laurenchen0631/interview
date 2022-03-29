8807cab8dde587e6011c5da936407b4fdef7513d
Lauren Chen <laurenchen0631@gmail.com>
2022-03-28 21:02:03 -0700

    287

---
+++ leetcode/python/*287_find_duplicate_number.py
@@ -0,0 +1,18 @@
+class Solution:
+    def findDuplicate(self, nums: list[int]) -> int:
+        slow = fast = nums[0]
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if slow == fast:
+                break
+        slow = nums[0]
+        while slow != fast:
+            slow = nums[slow]
+            fast = nums[fast]
+        return slow
+
+s = Solution()
+print(s.findDuplicate([1,3,4,2,2]))
+print(s.findDuplicate([3,1,3,4,2]))
+print(s.findDuplicate([2,2,2,3,4]))
