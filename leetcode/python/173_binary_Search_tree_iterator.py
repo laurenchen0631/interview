b394cb71e28e099f89c6bd3079d3cfc0f67f555a
Lauren Chen <laurenchen0631@gmail.com>
2022-04-19 22:23:26 -0700

    173

---
+++ leetcode/python/173_binary_Search_tree_iterator.py
@@ -0,0 +1,25 @@
+from turtle import st
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class BSTIterator:
+    def __init__(self, root: TreeNode | None):
+        self.root = root
+        self.stack: list[TreeNode] = []
+
+    def next(self) -> int:
+        while self.root:
+            self.stack.append(self.root)
+            self.root = self.root.left
+        self.root = self.stack.pop()
+        v = self.root.val
+        self.root = self.root.right
+        return v
+
+    def hasNext(self) -> bool:
+        return self.root or self.stack
