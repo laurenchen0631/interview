39588f576685c46abd054b862757d4a58baca497
Lauren Chen <laurenchen0631@gmail.com>
2022-05-06 09:44:37 -0700

    sql

---
+++ leetcode/sql/1633_percentage_of_users_attended_contest.sql
@@ -0,0 +1,26 @@
+Create table If Not Exists Users (user_id int, user_name varchar(20))
+Create table If Not Exists Register (contest_id int, user_id int)
+Truncate table Users
+insert into Users (user_id, user_name) values ('6', 'Alice')
+insert into Users (user_id, user_name) values ('2', 'Bob')
+insert into Users (user_id, user_name) values ('7', 'Alex')
+Truncate table Register
+insert into Register (contest_id, user_id) values ('215', '6')
+insert into Register (contest_id, user_id) values ('209', '2')
+insert into Register (contest_id, user_id) values ('208', '2')
+insert into Register (contest_id, user_id) values ('210', '6')
+insert into Register (contest_id, user_id) values ('208', '6')
+insert into Register (contest_id, user_id) values ('209', '7')
+insert into Register (contest_id, user_id) values ('209', '6')
+insert into Register (contest_id, user_id) values ('215', '7')
+insert into Register (contest_id, user_id) values ('208', '7')
+insert into Register (contest_id, user_id) values ('210', '2')
+insert into Register (contest_id, user_id) values ('207', '2')
+insert into Register (contest_id, user_id) values ('210', '7')
+
+SELECT
+  contest_id,
+  ROUND(COUNT(contest_id) / (SELECT COUNT(*) FROM Users) * 100, 2) AS percentage
+FROM Register
+GROUP BY contest_id
+ORDER BY percentage DESC, contest_id ASC;
