c37ccdfd029ebc5d468031f3405872f9ff9ab23d
Lorne Chen <laurenchen0631@gmail.com>
2022-09-01 12:55:56 -0700

    815

---
+++ leetcode/python/815_bus_routes.py
@@ -0,0 +1,35 @@
+from collections import defaultdict
+
+
+class Solution:
+    def numBusesToDestination(self, routes: list[list[int]], source: int, target: int) -> int:
+        if source == target: return 0
+        routes = list(map(set, routes))
+        print(routes)
+        graph = defaultdict(set)
+        for i, r1 in enumerate(routes):
+            for j in range(i+1, len(routes)):
+                r2 = routes[j]
+                if any(r in r2 for r in r1):
+                    graph[i].add(j)
+                    graph[j].add(i)
+        print(graph)
+
+        seen, targets = set(), set()
+        for node, route in enumerate(routes):
+            if source in route: seen.add(node)
+            if target in route: targets.add(node)
+        print(seen, targets)
+
+        queue = [(node, 1) for node in seen]
+        for node, depth in queue:
+            if node in targets: return depth
+            for nei in graph[node]:
+                if nei not in seen:
+                    seen.add(nei)
+                    queue.append((nei, depth+1))
+        return -1
+
+s = Solution()
+print(s.numBusesToDestination([[1,2,7],[3,6,7]], 1, 6))
+print(s.numBusesToDestination(routes = [[7,12],[4,5,15],[6],[15,19],[9,12,13]], source = 15, target = 12))
