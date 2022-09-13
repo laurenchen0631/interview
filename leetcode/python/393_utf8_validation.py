c85d657a1dc1ae3bcac90fe97f07f68758dbeaf5
Lorne Chen <laurenchen0631@gmail.com>
2022-09-13 14:38:11 -0700

    393

---
+++ leetcode/python/393_utf8_validation.py
@@ -0,0 +1,28 @@
+class Solution:
+    def validUtf8(self, data: list[int]) -> bool:
+        i: int = 0
+        while i < len(data):
+            n = self.getNumBytes(data[i])
+            if not n or (n == 1 and data[i] >> 7) or i + n > len(data):
+                return False
+            for j in range(i + 1, i + n):
+                if data[j] >> 6 != 2:
+                    return False
+            i += n
+        return True
+    
+    def getNumBytes(self, n: int) -> int:
+        if (n >> 3) == 30:
+            return 4
+        elif (n >> 4) == 14:
+            return 3
+        elif (n >> 5) == 6:
+            return 2
+        elif (n >> 7) == 0:
+            return 1
+        else:
+            return 0
+        
+s = Solution()
+print(s.validUtf8([197,130,1]))
+print(s.validUtf8([235,140,4]))
