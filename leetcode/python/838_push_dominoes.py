8c34b58103344cf4c5c545efe1c5f3cedc09955e
Lorne Chen <laurenchen0631@gmail.com>
2022-09-27 14:33:00 -0700

    838

---
+++ leetcode/python/838_push_dominoes.py
@@ -0,0 +1,17 @@
+class Solution:
+    def pushDominoes(self, dominoes: str) -> str:
+        n = len(dominoes)
+        force = [0] * n
+        f: int = 0
+        for i in range(n):
+            if dominoes[i] == 'R': f = n
+            elif dominoes[i] == 'L': f = 0
+            else: f = max(f-1, 0)
+            force[i] += f
+        for i in range(n-1, -1, -1):
+            if dominoes[i] == 'R': f = 0
+            elif dominoes[i] == 'L': f = n
+            else: f = max(f-1, 0)
+            force[i] -= f
+        return ''.join('.' if f == 0 else 'R' if f > 0 else 'L' for f in force)
+        
