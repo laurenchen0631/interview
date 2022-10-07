e508dca69dec4868476281d54feefeb8550be77f
Lorne Chen <laurenchen0631@gmail.com>
2022-10-06 20:02:00 -0700

    732

---
+++ leetcode/python/*732_my_calendar_III.py
@@ -0,0 +1,20 @@
+from sortedcontainers import SortedDict
+
+class MyCalendarThree:
+    def __init__(self):
+        self.diff = SortedDict()
+
+    def book(self, start: int, end: int) -> int:
+        self.diff[start] = self.diff.get(start, 0) + 1
+        self.diff[end] = self.diff.get(end, 0) - 1
+        cur = res = 0
+        for k in sorted(self.diff):
+            cur += self.diff[k]
+            res = max(res, cur)
+        return res
+        
+
+
+# Your MyCalendarThree object will be instantiated and called as such:
+# obj = MyCalendarThree()
+# param_1 = obj.book(start,end)
