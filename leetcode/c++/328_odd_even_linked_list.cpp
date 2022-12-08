ecaadff436d18864b6d2222651c8bb518762d2f4
Lorne Chen <laurenchen0631@gmail.com>
2022-12-07 22:16:19 -0800

    328, 872

---
+++ leetcode/c++/328_odd_even_linked_list.cpp
@@ -0,0 +1,26 @@
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if (!head) return head;
+        auto odd = head;
+        auto even = head->next;
+        auto evenHead = even;
+        while (even && even->next) {
+            odd->next = odd->next->next;
+            even->next = even->next->next;
+            odd = odd->next;
+            even = even->next;
+        }
+        odd->next = evenHead;
+        return head;
+    }
+};
