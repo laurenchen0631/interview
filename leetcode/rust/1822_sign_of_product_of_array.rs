17a43b8292efe8c30078f23b8577eb93e1b1f323
Lorne Chen <laurenchen0631@gmail.com>
2023-05-02 09:15:51 -0700

    1822

---
+++ leetcode/rust/1822_sign_of_product_of_array.rs
@@ -0,0 +1,13 @@
+impl Solution {
+    pub fn array_sign(nums: Vec<i32>) -> i32 {
+        let mut sign = 1;
+        for num in &nums {
+            if *num == 0 {
+                return 0;
+            } else if *num < 0 {
+                sign *= -1;
+            }
+        }
+        return sign;
+    }
+}
