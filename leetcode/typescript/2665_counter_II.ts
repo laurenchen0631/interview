1e6321ea9d6d12db6e7fe193999655a98b81a763
Lorne Chen <laurenchen0631@gmail.com>
2023-05-08 10:31:42 -0700

    2665

---
+++ leetcode/typescript/2665_counter_II.ts
@@ -0,0 +1,14 @@
+type ReturnObj = {
+    increment: () => number,
+    decrement: () => number,
+    reset: () => number,
+}
+
+function createCounter(init: number): ReturnObj {
+    let count = init;
+    return {
+        increment: () => ++count,
+        decrement: () => --count,
+        reset: () => count = init,
+    }
+};
