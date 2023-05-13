0ff4a40ae3d7e51ce09003deca40c8fb9237ac8d
Lorne Chen <laurenchen0631@gmail.com>
2023-05-13 09:41:57 -0700

    2466

---
+++ leetcode/python/2466_count_ways_to_build_good_strings.py
@@ -0,0 +1,15 @@
+class Solution:
+    def countGoodStrings(self, low: int, high: int, zero: int, one: int) -> int:
+        # Use dp[i] to record to number of good strings of length i.
+        dp = [1] + [0] * (high)
+        mod = 10 ** 9 + 7
+        
+        for end in range(1, high + 1):
+            if end >= zero:
+                dp[end] += dp[end - zero]
+            if end >= one:
+                dp[end] += dp[end - one]
+            dp[end] %= mod
+        
+        # Add up the number of strings with each valid length [low ~ high].
+        return sum(dp[low : high + 1]) % mod
