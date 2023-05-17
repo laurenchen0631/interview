2c917a9cb4f07035667a5f238144e04a4cab94ac
Lorne Chen <laurenchen0631@gmail.com>
2023-05-17 08:18:23 -0700

    2636

---
+++ leetcode/typescript/2636_promise_pool.ts
@@ -0,0 +1,18 @@
+type F = () => Promise<any>;
+
+async function promisePool(functions: F[], n: number): Promise<any> {
+    async function evaluateNext() {
+        if (functions.length === 0) return;
+        const fn = functions.shift()!;
+        await fn();
+        await evaluateNext();
+    }
+    const nPromises = Array(n).fill(0).map(evaluateNext);
+    await Promise.all(nPromises);
+};
+
+/**
+ * const sleep = (t) => new Promise(res => setTimeout(res, t));
+ * promisePool([() => sleep(500), () => sleep(400)], 1)
+ *   .then(console.log) // After 900ms
+ */
