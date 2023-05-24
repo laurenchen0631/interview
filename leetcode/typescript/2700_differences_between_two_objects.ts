0e3e7bb885ce28c68eea513847d058c825ea4ca6
Lorne Chen <laurenchen0631@gmail.com>
2023-05-24 11:04:40 -0700

    2700

--- leetcode/typescript/2700_differences_between_two_objects.ts
+++ leetcode/typescript/2700_differences_between_two_objects.ts
@@ -6,10 +6,7 @@ function objDiff(obj1: object | any[], obj2: object | any[]): object {
         if ((typeof obj1[key] !== 'object' || obj1[key] === null) && obj1[key] !== obj2[key]) {
             diff[key] = [obj1[key], obj2[key]];
         }
-        else if (
-            (Array.isArray(obj1[key]) && !Array.isArray(obj2[key])) ||
-            (Array.isArray(obj2[key]) && !Array.isArray(obj1[key]))
-        ) {
+        else if (Array.isArray(obj1[key]) !== Array.isArray(obj2[key])) {
             diff[key] = [obj1[key], obj2[key]];
         }
         else if (Array.isArray(obj1[key]) && Array.isArray(obj2[key])) {
