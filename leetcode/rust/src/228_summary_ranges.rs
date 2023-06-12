b3e0b4d2f2ca68a60941bbe9dbb204c6d7554822
Lorne Chen <laurenchen0631@gmail.com>
2023-06-12 08:27:13 -0700

    228

--- leetcode/rust/src/228_summary_ranges.rs
+++ leetcode/rust/src/228_summary_ranges.rs
@@ -4,28 +4,18 @@ pub fn summary_ranges(nums: Vec<i32>) -> Vec<String> {
         return ranges;
     }
 
-    let mut l = nums[0];
-    let mut r = nums[0];
-    for i in 1..nums.len() {
-        if nums[i] == r + 1 {
-            r = nums[i];
-        } else {
-            if l == r {
-                ranges.push(l.to_string());
-            } else {
-                ranges.push(format!("{}->{}", l, r));
-            }
-            l = nums[i];
-            r = nums[i];
+    let mut i = 0;
+    while i < nums.len() {
+        let mut j: usize = i;
+        while j < nums.len() - 1 && nums[j] + 1 == nums[j + 1] {
+            j += 1;
         }
+        if i == j {
+            ranges.push(nums[i].to_string());
+        } else {
+            ranges.push(format!("{}->{}", nums[i], nums[j]));
+        }
+        i = j + 1;
     }
-
-    if l == r {
-        ranges.push(l.to_string());
-    } else {
-        ranges.push(format!("{}->{}", l, r));
-    }
-
-
     ranges
 }
