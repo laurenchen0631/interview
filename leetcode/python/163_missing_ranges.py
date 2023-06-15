07cda669e476a9b746d9e508ac15e6fd39a51287
Lorne Chen <laurenchen0631@gmail.com>
2023-06-15 08:57:38 -0700

    163

--- leetcode/python/163_missing_ranges.py
+++ leetcode/python/163_missing_ranges.py
@@ -1,12 +1,10 @@
 class Solution:
-    def findMissingRanges(self, nums: list[int], lower: int, upper: int) -> list[str]:
+    def findMissingRanges(self, nums: list[int], lower: int, upper: int) -> list[list[int]]:
         prev = lower - 1
-        res: list[str] = []
+        res: list[list[int]] = []
         for n in nums + [upper+1]:
-            if n - prev > 2:
-                res.append(f'{prev+1}->{n-1}')
-            elif n - prev == 2:
-                res.append(f'{prev+1}')
+            if n - prev >= 2:
+                res.append([prev+1, n-1])
             prev = n
         return res
 
