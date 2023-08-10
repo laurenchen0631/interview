eda34c07d60151229d2ddeb1801f5d57618e9b77
Lorne Chen <laurenchen0631@gmail.com>
2023-08-10 09:17:59 -0700

    81

--- leetcode/python/*81_search_rotated_sorted_duplicate_array.py
+++ leetcode/python/*81_search_rotated_sorted_duplicate_array.py
@@ -1,22 +1,22 @@
 class Solution:
     def search(self, nums: list[int], target: int) -> bool:
-        l, r = 0, len(nums) - 1
+        l, r = 0, len(nums)-1
         while l <= r:
+            while l < r and nums[l] == nums[l+1]:
+                l += 1
+            while l < r and nums[r-1] == nums[r]:
+                r -= 1
+
             m = (l + r) // 2
             if nums[m] == target:
                 return True
-            if nums[l] == nums[r]:
-                if target == nums[l]:
-                    return True
-                l += 1
-                r -= 1
             elif nums[l] <= nums[m]:
-                if nums[l] <= target and target < nums[m]:
+                if nums[l] <= target < nums[m]:
                     r = m - 1
                 else:
                     l = m + 1
             else:
-                if target <= nums[r] and nums[m] < target:
+                if nums[m] < target <= nums[r]:
                     l = m + 1
                 else:
                     r = m - 1
