76823c3052c9179d65e3117c31d5610f75a3de73
Lorne Chen <laurenchen0631@gmail.com>
2023-09-17 09:13:26 -0700

    847

--- leetcode/python/847_shortest_path_visiting_nodes.py
+++ leetcode/python/847_shortest_path_visiting_nodes.py
@@ -1,15 +1,20 @@
+from functools import lru_cache
+from sys import maxsize
+
+
 class Solution:
     def shortestPathLength(self, graph):
-        def dp(node, mask):
+        @lru_cache(None)
+        def dp(node: int, mask: int) -> int:
             state = (node, mask)
             if state in cache:
                 return cache[state]
             if mask & (mask - 1) == 0:
                 # Base case - mask only has a single "1", which means
-                # that only one node has been visited (the current node)
+                # that only the current node is left
                 return 0
 
-            cache[state] = float("inf") # Avoid infinite loop in recursion
+            cache[state] = maxsize # Marked visited, Avoid infinite loop in recursion
             for neighbor in graph[node]:
                 if mask & (1 << neighbor):
                     already_visited = 1 + dp(neighbor, mask)
