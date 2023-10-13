2dc5b6dac97a8490f06c7d50ff3976cbfc6798fb
Lorne Chen <laurenchen0631@gmail.com>
2023-10-13 08:19:00 -0700

    746

---
+++ leetcode/typescript/746_min_cost_climbing_stairs.ts
@@ -0,0 +1,13 @@
+function minCostClimbingStairs(cost: number[]): number {
+    const n = cost.length;
+    const dp = Array(n +1).fill(0);
+
+    for (let i = 2; i <= n; i++) {
+        dp[i] = Math.min(
+            dp[i-2] + cost[i-2],
+            dp[i-1] + cost[i-1],
+        );
+    }
+
+    return dp[n];
+};
