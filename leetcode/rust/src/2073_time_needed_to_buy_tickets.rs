904d45ce99f4333afa6178bb7ceb960d117720ce
Lorne Chen <laurenchen0631@gmail.com>
2024-04-09 06:36:49 -0700

    2073

---
+++ leetcode/rust/src/2073_time_needed_to_buy_tickets.rs
@@ -0,0 +1,14 @@
+impl Solution {
+    pub fn time_required_to_buy(tickets: Vec<i32>, k: i32) -> i32 {
+        let mut res = 0;
+        for i in 0..tickets.len() {
+            if i <= k as usize {
+                res += tickets[i].min(tickets[k as usize]);
+            }
+            else {
+                res += tickets[i].min(tickets[k as usize] - 1);
+            }
+        }
+        res
+    }
+}
