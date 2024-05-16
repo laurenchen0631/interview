afeb4e42a1f1d1a47881ea9d37539742ee6f17cb
Lorne Chen <laurenchen0631@gmail.com>
2024-05-16 08:34:34 -0700

    2331

---
+++ leetcode/python/2331_evaluate_boolean_binary_tree.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def evaluateTree(self, root: TreeNode | None) -> bool:
+        if not root:
+            return False
+        if not root.left and not root.right:
+            return root.val == 1
+
+        left = self.evaluateTree(root.left)
+        right = self.evaluateTree(root.right)
+        if root.val == 2:
+            return left or right
+        if root.val == 3:
+            return left and right
+        
